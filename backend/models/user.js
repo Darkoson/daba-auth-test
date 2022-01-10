92f3f2b02187dba07c07a3f505e2d8e39f7dcaae
Darkoson <darkothomas80@gmail.com>
2022-01-10 04:01:41 +0000

    creation of the user entity

---
+++ backend/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  photo: {
+    type: String,
+    require: true,
+  },
+  name: {
+    type: String,
+    require: true,
+  },
+  bio: {
+    type: String,
+    require: true,
+  },
+  phone: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
